e42da1474408a79ca2b16a65b283adafe9a8fb03
pardeepky <pardeep.pky@gmail.com>
2023-02-22 12:19:45 +0530

    functionaly for opening and closing modal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Layout/Header'
 import AvailableMeals from './components/Meals/AvailableMeals'
 import MealSummary from './components/Meals/MealSummary'
 import Cart from './components/Cart/Cart'
 
 const App = () => {
+  const [showCart, setShowCart] = useState(false)
+
+  const showCartHandler = () => {
+    setShowCart(true);
+  }
+
+  const hideCartHandler = () => {
+    setShowCart(false);
+  }
   return (
     <>
-    <Cart />
-      <Header />
+    {showCart && <Cart hideCartHandler={hideCartHandler}/>}
+      <Header showCartHandler={showCartHandler}/>
       <main>
         <MealSummary />
         <AvailableMeals />
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,12 +3,12 @@ import mealImg from '../../assets/meals.jpg'
 import classes from './Header.module.css'
 import HeaderCartButton from './HeaderCartButton'
 
-const Header = () => {
+const Header = ({showCartHandler}) => {
     return (
         <>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <HeaderCartButton />
+                <HeaderCartButton  showCartHandler={showCartHandler}/>
             </header>
             <div className={classes['main-image']}>
                 <img src={mealImg} alt="A table full of delicious food" />
