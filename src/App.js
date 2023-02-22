c075f3b460045d7a453bfd7482abae003c3cce28
pardeepky <pardeep.pky@gmail.com>
2023-02-22 10:57:52 +0530

    added summary for meals

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/Layout/Header'
+import MealSummary from './components/Meals/MealSummary'
 
 const App = () => {
   return (
     <>
       <Header />
+      <MealSummary />
     </>
   )
 }
