e169a2d8c83db28618c8f9a451c67cdf563de0a4
pardeepky <pardeep.pky@gmail.com>
2023-02-22 12:09:04 +0530

    added static cart component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Header from './components/Layout/Header'
 import AvailableMeals from './components/Meals/AvailableMeals'
 import MealSummary from './components/Meals/MealSummary'
+import Cart from './components/Cart/Cart'
 
 const App = () => {
   return (
     <>
+    <Cart />
       <Header />
       <main>
         <MealSummary />
